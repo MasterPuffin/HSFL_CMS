0ea741cfc454827539cdcb01adb5c6f2fa83d288
Johannes <johannes.bluhm@outlook.com>
2021-05-18 17:07:47 +0200

    fucking working

--- web/modules/custom/bljo6035_new/src/Controller/NewController.php
+++ web/modules/custom/bljo6035_new/src/Controller/NewController.php
@@ -11,9 +11,28 @@ use Drupal\Core\Controller\ControllerBase;
 
 class NewController extends ControllerBase {
   public function new(string $name = ""): array {
+    $nids = \Drupal::entityQuery('node')->condition('status', 1)->condition('type', 'rezept')->execute();
+/*
+    $query = \Drupal::entityQuery('node')
+      ->condition('status', 1)
+      ->condition('type', 'rezept');
+
+
+    $nids = $query->execute();
+    */
+    //Flip array to have the newest items first and get the first three items
+    $nids = array_slice($nids, 0, 3);
+
+    $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
+    //
+    //echo "<pre>";
+    //print_r($nodes);
+    //echo "</pre>";
+
+
     return array(
       '#theme' => 'new',
-      '#text' => t('Hello ' . (empty($name) ? 'World' : $name)),
+      '#text' => $nodes,
     );
   }
 }
