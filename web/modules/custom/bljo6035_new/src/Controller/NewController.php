cdb4ba15fa26100e13d9596c7596c93c27ab6eb3
Johannes <johannes.bluhm@outlook.com>
2021-05-18 17:11:33 +0200

    cleanup

--- web/modules/custom/bljo6035_new/src/Controller/NewController.php
+++ web/modules/custom/bljo6035_new/src/Controller/NewController.php
@@ -10,29 +10,10 @@ namespace Drupal\bljo6035_new\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class NewController extends ControllerBase {
-  public function new(string $name = ""): array {
-    $nids = \Drupal::entityQuery('node')->condition('status', 1)->condition('type', 'rezept')->execute();
-/*
-    $query = \Drupal::entityQuery('node')
-      ->condition('status', 1)
-      ->condition('type', 'rezept');
-
-
-    $nids = $query->execute();
-    */
-    //Flip array to have the newest items first and get the first three items
-    $nids = array_slice($nids, 0, 3);
-
-    $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
-    //
-    //echo "<pre>";
-    //print_r($nodes);
-    //echo "</pre>";
-
-
+  public function new(): array {
     return array(
       '#theme' => 'new',
-      '#text' => $nodes,
+      '#text' => bljo6035_new_getLatestRecipes(),
     );
   }
 }
